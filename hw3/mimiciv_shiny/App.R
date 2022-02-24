d0c9d7182d1d4891c940ceb663bcf2055c54a617
khyeh0816 <khyeh0816@g.ucla.edu>
2022-02-24 06:36:49 +0000

    Build simple version of app

--- hw3/mimiciv_shiny/App.R
+++ hw3/mimiciv_shiny/App.R
@@ -1,39 +1,82 @@
 # Load packages ----
 library(shiny)
 library(shinythemes)
+library(tidyverse)
 library(dplyr)
 library(readr)
 
 # Load data
-icu_cohort <- readRDS("hw3/mimiciv_shiny/icu_cohort.rds")
+setwd("/home/khyeh0816/biostat-203b-2022-winter/hw3/mimiciv_shiny")
+icu_cohort <- readRDS("icu_cohort.rds")
+
+#select demographics, lab measurements, vitals
+icu_cohort <- icu_cohort %>% 
+  select(c(5,6,9,10,11,12,17:23,27:43))
+icu_cohort$thirty_day_mort <- as.character(icu_cohort$thirty_day_mort)
+
 
 # Define UI
 ui <- fluidPage(
   theme = shinytheme("journal"),
   
   titlePanel("Summary Statistics & Graphs for ICU cohort"),
+  
     sidebarLayout(
       sidebarPanel(
-        helpText("Select a variable to have a deeper understanding."),         
-        selectInput("region", "Region:", 
-                    choices=colnames(WorldPhones)),
+        helpText("Select a variable to have a deeper understanding."),
+        varSelectInput("variable", "Choose a variable:", icu_cohort),
         hr(),
         helpText("Data from Medical Information Mart for Intensive Care")
       ),
-      
-
+      # Show a bar plot or box plot and summary stat
+      mainPanel(
+        
+        plotOutput("Plot"),
+        tableOutput("Contents"),
+        verbatimTextOutput("Summary")
+        
+      )
+    )
+)
 
 # Define server function
-
-
-
-
-
-
-
-
-
-
+server <- function(input, output){
+  
+  var <- reactive({
+    var_select <- icu_cohort %>%
+      select(input$variable)
+  })
+  
+  output$Plot <- renderPlot({
+    if (summary(var())[2] == "Class :character  "){
+      ggplot(data = icu_cohort) + 
+        geom_bar(mapping = aes_string(x = input$variable, 
+                                      fill = input$variable))
+                     
+    }else {
+      ggplot(data = icu_cohort) + 
+        geom_boxplot(mapping = aes_string(x = input$variable), 
+                     color = "burlywood2", 
+                     outlier.shape = NA)
+    }
+    
+  })
+  
+  
+  output$Contents <- renderTable({
+    head(var())
+  })
+  
+  output$Summary <- renderPrint({
+    if (summary(var())[2] == "Class :character  "){
+      table(var())
+    }else{
+      summary(var())
+    }
+  })
+  
+ 
+}
 
 # Create Shiny object
 shinyApp(ui = ui, server = server)
